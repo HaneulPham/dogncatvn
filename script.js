d46fa626225b87708a1446796a41e70df8b65944
Haneul Pham <haneul.pham@gmail.com>
2024-11-21 17:32:04 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Display a welcome message when the page loads
+alert("Welcome to My First Website!");
+
+// Add an interaction when the heading is clicked
+document.querySelector('h1').addEventListener('click', function() {
+    alert("You clicked on the heading!");
+});
